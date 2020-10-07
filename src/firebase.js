60de3d8a2dda2a9ab1e4e9cef5b902a218f12051
unknown <shmoon2917@naver.com>
2020-10-07 16:09:02 +0900

    git ignore 추가

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,5 @@
 import firebase from "firebase";
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAUptSeOCUEJZvR59y6exHPY-ndOx-0n9M",
-  authDomain: "instagram-clone-249b5.firebaseapp.com",
-  databaseURL: "https://instagram-clone-249b5.firebaseio.com",
-  projectId: "instagram-clone-249b5",
-  storageBucket: "instagram-clone-249b5.appspot.com",
-  messagingSenderId: "843987064638",
-  appId: "1:843987064638:web:eda6fdd6ce8a8169e8be6f",
-  measurementId: "G-VD3LG8GTQJ",
-};
+import firebaseConfig from "./config";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
